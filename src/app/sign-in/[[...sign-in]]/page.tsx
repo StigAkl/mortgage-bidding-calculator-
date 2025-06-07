eeb08ef86833050f1cf21cffb7300daa639139af
stigakl <stigakl@outlook.com>
2025-06-08 01:51:22 +0200

    init

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,11 @@
+import Container from '@/components/Container'
+import { SignIn } from '@clerk/nextjs'
+
+export default function Page() {
+  return (
+    <Container className="flex justify-center">
+      <SignIn />
+    </Container>
+  )
+
+}
---
+++ src/components/Container.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils";
+import { ComponentProps } from "react"
+
+interface ContainerProps extends ComponentProps<"div"> {
+  className?: string;
+}
+
+const Container = ({ children, className }: ContainerProps) => {
+  return (
+    <div className={cn('max-w-5xl mx-auto px-5', className)}>
+      {children}
+    </div>
+  )
+}
+
+export default Container;
